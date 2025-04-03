f0baa6a0f33f5252eea71bd4cd6f330fc4cfe5af
IoanaT22 <ioanatutica22@gmail.com>
2025-04-03 17:14:04 +0300

    update file

--- exercitiul01.c
+++ exercitiul01.c
@@ -1,82 +1,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include<malloc.h>
+#include <malloc.h>
 
-struct Magazin {
-    int cod;
-    char* denumire;
-    int nrProduse;
-    float* preturi; 
+struct Tir {
+	int cod;
+	char* denumire;
+	int nrMarfuri;
+	float* greutati;
 };
-typedef struct Magazin Magazin;
 
-Magazin citireMagazin() {
-    Magazin m;
-    char buffer[100];
-    
-    printf("Introduceti codul magazinului: ");
-    scanf("%d", &m.cod);
+struct Tir initializare(int cod, const char* denumire, int nrMarfuri, float* greutati) {
+	struct Tir t;
+	t.cod = cod;
+	t.denumire = (char*)malloc(strlen(denumire) + 1);
+	strcpy_s(t.denumire, strlen(denumire) + 1, denumire);
 
-    printf("Introduceti denumirea magazinului: ");
-    scanf("%s", buffer);
-    m.denumire = malloc(strlen(buffer) + 1);
-    strcpy(m.denumire, buffer);
-
-    printf("Introduceti numarul de produse: ");
-    scanf("%d", &m.nrProduse);
-
-    m.preturi = malloc(m.nrProduse * sizeof(float));
-    printf("Introduceti preturile pentru fiecare produs:\n");
-    for (int i = 0; i < m.nrProduse; i++) {
-        printf("Pretul produsului %d: ", i + 1);
-        scanf("%f", &m.preturi[i]);
-    }
-
-    return m;
+	t.nrMarfuri = nrMarfuri;
+	t.greutati = (float*)malloc(sizeof(float) * nrMarfuri);
+	for (int i = 0; i < nrMarfuri; i++) {
+		t.greutati[i] = greutati[i];
+	}
+	return t;
 }
 
-float calculeazaMediaPreturilor(Magazin m) {
-    float suma = 0;
-    for (int i = 0; i < m.nrProduse; i++) {
-        suma += m.preturi[i];
-    }
-    return suma / m.nrProduse;
+void afisare(struct Tir t) {
+	printf("Cod TIR: %d\n", t.cod);
+	printf("Denumire: %s\n", t.denumire);
+	printf("Numar marfuri: %d\n", t.nrMarfuri);
+	for (int i = 0; i < t.nrMarfuri; i++) {
+		printf("Greutate marfa %d: %.2f\n", i + 1, t.greutati[i]);
+	}
 }
 
-void modificaPret(Magazin* m, int indexProdus, float noulPret) {
-    if (indexProdus >= 0 && indexProdus < m->nrProduse) {
-        m->preturi[indexProdus] = noulPret;
-    } else {
-        printf("Indexul produsului este invalid.\n");
-    }
+float calculeazaGreutateTotala(struct Tir t) {
+	float suma = 0;
+	for (int i = 0; i < t.nrMarfuri; i++) {
+		suma += t.greutati[i];
+	}
+	return suma;
 }
 
-void afiseazaMagazin(Magazin m) {
-    printf("Cod magazin: %d\n", m.cod);
-    printf("Denumire magazin: %s\n", m.denumire);
-    printf("Numar produse: %d\n", m.nrProduse);
-    printf("Preturi produse:\n");
-    for (int i = 0; i < m.nrProduse; i++) {
-        printf("Produs %d: %.2f\n", i + 1, m.preturi[i]);
-    }
+void modificaGreutate(struct Tir* t, int index, float nouaGreutate) {
+	if (index >= 0 && index < t->nrMarfuri) {
+		t->greutati[index] = nouaGreutate;
+	}
+	else {
+		printf("Index invalid!\n");
+	}
+}
+
+struct Tir citireTir() {
+	struct Tir t;
+	char buffer[100];
+
+	printf("Introduceti codul TIR-ului: ");
+	scanf("%d", &t.cod);
+
+	printf("Introduceti denumirea TIR-ului: ");
+	scanf("%s", buffer);
+	t.denumire = (char*)malloc(strlen(buffer) + 1);
+	strcpy_s(t.denumire, strlen(buffer) + 1, buffer);
+
+	printf("Introduceti numarul de marfuri: ");
+	scanf("%d", &t.nrMarfuri);
+
+	t.greutati = (float*)malloc(t.nrMarfuri * sizeof(float));
+	for (int i = 0; i < t.nrMarfuri; i++) {
+		printf("Introduceti greutatea marfii %d: ", i + 1);
+		scanf("%f", &t.greutati[i]);
+	}
+
+	return t;
+}
+
+void dezalocare(struct Tir* t) {
+	free(t->denumire);
+	free(t->greutati);
+	t->denumire = NULL;
+	t->greutati = NULL;
+	t->nrMarfuri = 0;
 }
 
 int main() {
-    Magazin m = citireMagazin();
-    
-    float media = calculeazaMediaPreturilor(m);
-    printf("Media preturilor produselor: %.2f\n", media);
-    int produsDeModificat;
-    float nouPret;
-    printf("Introduceti indexul produsului de modificat (0-%d) si noul pret: ", m.nrProduse - 1);
-    scanf("%d %f", &produsDeModificat, &nouPret);
-    modificaPret(&m, produsDeModificat, nouPret);
-    afiseazaMagazin(m);
-    free(m.denumire);
-    free(m.preturi);
-    
-    return 0;
+	struct Tir tir = citireTir();
+
+	printf("\nAfisare TIR:\n");
+	afisare(tir);
+
+	float total = calculeazaGreutateTotala(tir);
+	printf("Greutate totala: %.2f\n", total);
+
+	int index;
+	float nouaGreutate;
+	printf("Introduceti indexul marfii de modificat (0 - %d) si noua greutate: ", tir.nrMarfuri - 1);
+	scanf("%d %f", &index, &nouaGreutate);
+	modificaGreutate(&tir, index, nouaGreutate);
+
+	printf("\nDupa modificare:\n");
+	afisare(tir);
+
+	dezalocare(&tir);
+
+	return 0;
 }
-
-
